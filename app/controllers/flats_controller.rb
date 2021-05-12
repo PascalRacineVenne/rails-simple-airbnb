2dd844e312ee0d197b89ba44df8e783847566288
Pascal Racine-Venne <pascalracinevenne@gmail.com>
2021-05-12 16:27:13 -0400

    controller well on it's way

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,40 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def flat_params
+      params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+    end
+
+    def set_flat
+      @flat = Flat.find(params[:id])
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning the DB..."
+Flat.destroy_all
+
+puts "Creating 4 flats..."
+
+4.times do
+  flat = Flat.create!(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.sentence,
+    price_per_night: rand(50..150),
+    number_of_guests: rand(2..10)
+  )
+  puts "flat #{flat.id} was created"
+end
+
+puts "All done!"
+
